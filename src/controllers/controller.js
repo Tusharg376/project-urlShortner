0e055cfa7b1de0f4a43fa426d78b22df65d96227
Sandyvaranasi <tripathisn5363@gmail.com>
2023-01-19 15:20:06 +0530

    project3-phase2

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,6 +1,8 @@
 const model = require("../models/urlModel");
 
-const isValidUrl = require("url-validation");
+const axios = require("axios");
+
+const isValidUrl = require('url-validation')
 
 const shortId = require("shortid");
 
@@ -9,22 +11,36 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 const redisClient = redis.createClient(
-    16948,
-    'redis-16948.c305.ap-south-1-1.ec2.cloud.redislabs.com',
-     {no_ready_check : true}
-);
-redisClient.auth(
-    '6DLOlO0DrbeI2rubVd34Ipg74aQeXhdj',
-    function(err){
-        if(err) throw err;
-    }
-);
-redisClient.on("connect", async function () {
-    console.log("Connected to Redis..");
-  });
 
-  const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
-  const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
+    16948,
+
+    'redis-16948.c305.ap-south-1-1.ec2.cloud.redislabs.com',
+
+    { no_ready_check: true }
+
+);
+
+redisClient.auth(
+
+    '6DLOlO0DrbeI2rubVd34Ipg74aQeXhdj',
+
+    function (err) {
+
+        if (err) throw err;
+
+    }
+
+);
+
+redisClient.on("connect", async function () {
+
+    console.log("Connected to Redis..");
+
+});
+
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
 
 
 const createUrl = async function (req, res) {
@@ -33,32 +49,40 @@ const createUrl = async function (req, res) {
 
         let data = req.body;
 
-        if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "please enter URL" });
+        if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "please enter valid URL" });
 
         data.longUrl = data.longUrl.trim();
 
         if (Object.values(data) == "") return res.status(400).send({ status: false, message: "Entry field is empty" });
 
-        if (!isValidUrl(data.longUrl)) return res.status(400).send({ status: false, message: "invalid URL" });
-
-        
         let alreadyCreated = await GET_ASYNC(data.longUrl);
 
-        let data1 = JSON.parse(alreadyCreated);
+        if (alreadyCreated) {
 
-        if(alreadyCreated){
-             return res.status(200).send({status:true,data:data1});
-            }
+            return res.status(200).send({ status: true, data: JSON.parse(alreadyCreated) });
+
+        }
+
+        if (!isValidUrl(data.longUrl)) return res.status(400).send({ status: false, message: "invalid URL" });
 
         const uniqueUrl = await model.findOne(data).select({ _id: 0, __v: 0, createdAt: 0, updatedAt: 0 });
 
-        
+        if (uniqueUrl) {
 
-        if(uniqueUrl){
-            await SETEX_ASYNC(data.longUrl,30,JSON.stringify(uniqueUrl));
+            await SETEX_ASYNC(data.longUrl, 86400, JSON.stringify(uniqueUrl));
+
             return res.status(200).send({ status: true, data: uniqueUrl })
-    }
-       
+
+        }
+
+        let genuineUrl = await axios.get(data.longUrl)
+
+            .then(() => data.longUrl)
+
+            .catch((err) => null)
+
+        if (!genuineUrl) return res.status(404).send({ status: false, message: "Page not found" })
+
         data.urlCode = shortId.generate();
 
         data.urlCode = data.urlCode.toLowerCase().trim()
@@ -69,7 +93,7 @@ const createUrl = async function (req, res) {
 
         let { urlCode, longUrl, shortUrl } = shorterUrl
 
-        await SETEX_ASYNC(`${data.longUrl}`,30, JSON.stringify({ status: true, data: { urlCode, longUrl, shortUrl } }))
+        await SETEX_ASYNC(`${data.longUrl}`, 86400, JSON.stringify( { urlCode, longUrl, shortUrl } ))
 
         res.status(201).send({ status: true, data: { urlCode, longUrl, shortUrl } });
 
@@ -93,9 +117,9 @@ const getUrl = async function (req, res) {
 
         let alreadyCreated = await GET_ASYNC(url);
 
-        if(alreadyCreated) {
-            
-           let redirectUrl =  alreadyCreated.replaceAll('"','' )
+        if (alreadyCreated) {
+
+            let redirectUrl = alreadyCreated.replaceAll('"', '')
 
             return res.status(302).redirect(redirectUrl)
 
@@ -105,7 +129,7 @@ const getUrl = async function (req, res) {
 
         if (!longUrl) return res.status(404).send({ status: false, message: "URL not found" });
 
-        await SETEX_ASYNC(`${url}`,30, JSON.stringify(longUrl.longUrl));
+        await SETEX_ASYNC(`${url}`, 86400, JSON.stringify(longUrl.longUrl));
 
         return res.status(302).redirect(longUrl.longUrl)
 
@@ -116,7 +140,6 @@ const getUrl = async function (req, res) {
     }
 
 }
-
 
 module.exports.createUrl = createUrl;
 
